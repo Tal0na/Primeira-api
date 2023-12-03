ecf46a193b66891aa6f4fa5da3a5bb327b9cf35e
Talonacosta <talonacontato@gmail.com>
2023-12-03 02:14:31 -0300

    format

--- src/pages/api/tempo.jsx
+++ src/pages/api/tempo.jsx
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
-  const dynamicDate = new Date();
+  const dynamicDate = new Date()
+  const options = { timeZone: "America/Sao_Paulo" }
+  const formattedDate = dynamicDate.toLocaleString("pt-BR", options)
 
   res.json({
-    date: dynamicDate.toGMTString(),
-  });
+    date: formattedDate,
+  })
 }
